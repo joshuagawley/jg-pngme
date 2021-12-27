4234e5cd7bbfba9acf4f58c2cd0069bae48b7f64
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2021-12-27 23:00:08 +0000

    Initial commit
    Signed-off-by: Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+mod args;
+mod chunk;
+mod chunk_type;
+mod commands;
+mod png;
+
+pub type Error = anyhow::Error;
+pub type Result<T> = anyhow::Result<T>;
+
+fn main() -> Result<()> {
+    todo!()
+}
+
